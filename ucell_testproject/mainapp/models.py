3f474e97e30ae754abe5614c3729503f384c4be8
alexagapkin <alexthemaggot23@gmail.com>
2022-09-20 02:59:35 +0500

    add products model

--- ucell_testproject/mainapp/models.py
+++ ucell_testproject/mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Цена')
+    color = models.CharField(max_length=200, verbose_name='Цвет')
+    quantity = models.IntegerField(verbose_name='Количество')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
